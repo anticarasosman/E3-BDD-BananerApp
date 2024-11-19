1cd8e5d316b0c962caa73d2c6172aa420982b63a
NOMBRE APELLIDO <correo@ejemplo.com>
2024-11-19 15:11:27 -0300

    casc

--- BananerApp/src/consultas/procesar_consulta.php
+++ BananerApp/src/consultas/procesar_consulta.php
@@ -56,46 +56,85 @@ $condicion = $_POST['condicion'];
 
 
 
-
-
+ $sql = "SELECT $columna FROM $tabla";
+ $parametro = [];
  if (!empty($condicion)){
 
-    $oper = '/^([\w]+)\s*(=|>|<|>=|<=|LIKE|IN|IS NULL|IS NOT NULL|EXISTS|NOT EXISTS|BETWEEN|)\s*(.*)$/i';
+    $oper = '/^([\w]+)\s*(=|>=|<=|>|<|LIKE|IN|IS NULL|IS NOT NULL|EXISTS|NOT EXISTS|BETWEEN)\s*(.+)$/i';
 
     if (preg_match($oper,$condicion,$matches)){
+
+        var_dump($matches);
         $colum_condic = $matches[1];
         $operador = strtoupper(trim($matches[2]));
-        echo($operador);
+        
         $valorr = trim($matches[3]);
+        
+       
 
+
+
+        if (!in_array($colum_condic,$validos_c)){
+                echo($validos_c);
+                die("ERROR COLUMNAa '$colum_condic' No es valida");
+            }
+
+
+
+
+        if (in_array($operador,['IN','NOT IN'])){
+            if (!preg_match('/^\(.+\)$/',$valorr)){
+                die("ERROR: operador debe estar entre parentesis");
+            }
+                $sql = "SELECT $columna FROM $tabla WHERE  $colum_condic $operador $valorr";
+
+
+        }elseif (in_array($operador,['IS NULL',' IS NOT NULL'])){
+                if (!empty(($valorr))){
+                    die("ERROR ESTE OPERADOR NO DEBE TENER VALOR");
+                }
+                $sql = "SELECT $columna FROM $tabla WHERE  $colum_condic $operador";
+        }elseif ($operador === "BETWEEN"){
+
+
+            if (!preg_match('/^.+\s+AND\s+.+$/i',$valorr)){
+                    die("ERROR CON FORMATO");
+            }
+
+            $sql = "SELECT $columna FROM $tabla WHERE  $colum_condic $operador :valor";
+
+            $parametro[':valor']  = $valorr;
+
+
+
+        } else{
+            $sql =  "SELECT $columna FROM $tabla WHERE  $colum_condic $operador :valor";
+            $parametro[':valor']  = $valorr;
+                
 
     }
 
-    if (!in_array($colum_condic,$validos_c)){
-        echo($validos_c);
-        die("ERROR COLUMNAa '$colum_condic' No es valida");
     }
-
-
-
-
- }
-
-$sql = "SELECT $columna FROM $tabla WHERE  $colum_condic $operador $valorr";
-
-
-try{
-    $query = $db->prepare($sql);
-    $query->execute();
-    $resultados = $query->fetchAll(PDO::FETCH_ASSOC);
-
 
 
     
 
+ }
 
 
+try{
+    $query = $db->prepare($sql);
+    
+    
 
+
+    foreach($parametro as $clave => $valor){
+        $query->bindParam($clave,$valor,PDO::PARAM_STR);
+    }
+        
+
+    $query->execute();
+    $resultados = $query->fetchAll(PDO::FETCH_ASSOC);
     echo "<h1>Resultados </h1>";
 
     if ($resultados){
